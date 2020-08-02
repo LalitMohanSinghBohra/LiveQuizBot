6d5c97ab63d1beb97500b350c5cca1601d6daa69
LalitMohanSinghBohra <61799336+LalitMohanSinghBohra@users.noreply.github.com>
2020-08-03 01:13:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pip3 install -r requirements.txt
-python3 -m nltk.downloader stopwords 
-python3 -m nltk.downloader punkt
-python3 -m spacy download it_core_news_sm
+py -m nltk.downloader stopwords 
+py -m nltk.downloader punkt
+py -m spacy download it_core_news_sm
 
 brew install tesseract
 brew install tesseract-lang
